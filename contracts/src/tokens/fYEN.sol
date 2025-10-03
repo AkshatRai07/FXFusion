f86ed18fe6d329b0f8c4d9a4d45c974d44b88fa1
AkshatRai07 <sanamrai2007@gmail.com>
2025-10-03 09:28:54 +0530

    Fixed ERC20 contracts

--- contracts/src/tokens/fYEN.sol
+++ contracts/src/tokens/fYEN.sol
@@ -2,33 +2,41 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract fYEN is ERC20 {
-    uint256 public constant SCALE = 1e18;
+contract fYEN is ERC20, Ownable {
+    address public appContract;
 
-    constructor() ERC20("flowYEN", "fYEN") {
-        _mint(address(this), 1_000_000 * 10 ** decimals());
+    constructor() ERC20("flowYEN", "fYEN") Ownable(msg.sender) {
+        _mint(address(this), 10 * 10 ** 18 * 10 ** decimals()); // 10 quintillion fYEN
     }
 
-    function buyTokens(uint256 rate) external payable {
+    modifier onlyAuthorized() {
+        require(msg.sender == owner() || msg.sender == appContract, "Not authorized");
+        _;
+    }
+
+    function setAppContract(address _appContract) external onlyOwner {
+        require(_appContract != address(0), "Invalid App contract");
+        appContract = _appContract;
+    }
+
+    function buyTokens(uint256 rate) external payable onlyAuthorized {
         require(msg.value > 0, "Send ETH to buy fYEN");
 
-        uint256 tokenAmount = (msg.value * rate) / SCALE;
-        require(balanceOf(address(this)) >= tokenAmount, "Not enough fINR in contract");
+        uint256 tokenAmount = msg.value * rate;
+        require(balanceOf(address(this)) >= tokenAmount, "Not enough fYEN in contract");
 
         _transfer(address(this), msg.sender, tokenAmount);
     }
 
-    function sellTokens(uint256 tokenAmount, uint256 rate) external {
+    function sellTokens(uint256 tokenAmount, uint256 rate) external onlyAuthorized {
         require(tokenAmount > 0, "Must sell > 0 tokens");
 
-        uint256 ethAmount = (tokenAmount * SCALE) / rate;
+        uint256 ethAmount = tokenAmount / rate;
         require(address(this).balance >= ethAmount, "Not enough ETH liquidity");
 
         _transfer(msg.sender, address(this), tokenAmount);
-
-        (bool success, ) = payable(msg.sender).call{ value:ethAmount }("");
-
-        require(success, "Transaction unsuccessfull");
+        payable(msg.sender).transfer(ethAmount);
     }
 }
